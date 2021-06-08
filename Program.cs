706a37ed9dd7e4fca43ae87a6533f30ae4468be3
PeterKirillow <peter.kirillow@gmail.com>
2021-06-08 22:10:39 +0300

    -

--- Program.cs
+++ Program.cs
@@ -8,28 +8,28 @@ namespace Matrix
         /****************************************************************************/
         static void Main()
         {
-            // сложение/вычитание матриц
             //Console.WriteLine("сложение");
             //M1 M1_1 = new M1(1);
 
             //Console.WriteLine("вычитание");
             //M1 M1_2 = new M1(2);
 
-            // умножение матрицы на число
             //Console.WriteLine("умножение матрицы на число");
             //M2 M2_1 = new M2("A");
 
             // обратная матрица - http://mathprofi.ru/kak_naiti_obratnuyu_matricu.html
             //Console.WriteLine("нахождение обратной матрицы");
-            //M3 M3_1 = new M3("D11");
-            //M3 M3_1 = new M3("D11");
-            M3 M3_1 = new M3(10, 10, 10);
-            M3_1.calc();
+            // simple
+            //M3 M3_1 = new M3("D3");
+            //M3 M3_2 = new M3("D4");
 
-            // перемножение матриц
+            // parallel - это не показываем ))
+            //M3P M3P_1 = new M3P(10, 10, 10);
+            //M3P_1.calc();
+
             //Console.WriteLine("перемножение матриц");
             //M4 M4_1 = new M4("C1","C2");
-            //M4 M4_1 = new M4("C3", "C5");
+            //M4 M4_2 = new M4("C3", "C5");
         }
     }
 }
--- _matrix.cs
+++ _matrix.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-using Newtonsoft.Json;
 
 namespace Matrix
 {
